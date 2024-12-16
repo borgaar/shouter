f43534b58f7cc8d6aed685e21f65f73abda0635b
Borgar Barland <142639383+borgaar@users.noreply.github.com>
2024-12-16 22:35:11 +0100

    created initial shout view

--- src/app/_components/shout.tsx
+++ src/app/_components/shout.tsx
@@ -1,17 +1,25 @@
 export default function Shout({
+  id,
   user: { name, image },
   content,
   likes,
   createdAt,
 }: {
+  id: string;
   user: { name: string; image: string };
   content: string;
   likes: number;
   createdAt: Date;
 }) {
   return (
-    <div className="items-center justify-start">
-      <img className="rounded-full" src={image} alt={name} />
+    <div className="flex w-full flex-row items-center justify-start rounded-lg border-2 border-black bg-secondary p-4 text-black">
+      <img className="h-16 rounded-full" src={image} alt={name} />
+      <div className="flex flex-col px-4">
+        <p className="text-slate-600">
+          {createdAt.toLocaleDateString()} {createdAt.toLocaleTimeString()}
+        </p>
+        <div>{content}</div>
+      </div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 import { auth } from "~/server/auth";
-import { api, } from "~/trpc/server";
+import { api } from "~/trpc/server";
 import Shout from "./_components/shout";
 import CreateShout from "./_components/create_shout";
 
@@ -11,7 +11,7 @@ export default async function Home() {
   const shouts = await api.post.getShouts(0); // TODO: pagination logic
 
   return (
-    <main className="bg-primary_dark flex min-h-screen flex-col items-center justify-center text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-primary">
       <div>
         {session ? (
           <Link href="/api/auth/signout">Sign out</Link>
@@ -19,10 +19,14 @@ export default async function Home() {
           <Link href="/api/auth/signin">Sign in</Link>
         )}
       </div>
-      {session && (<CreateShout/>
-        )}
-      <div className="bg-primary container flex h-screen max-w-screen-md flex-col items-center justify-center gap-12 px-4 py-16">
-        {shouts.map((s) => Shout(s))}
+      {session && <CreateShout key={"create-shout"} />}
+      <div
+        key={"shout-view"}
+        className="container flex h-screen max-w-screen-md flex-col items-center gap-8 px-4 py-16"
+      >
+        {shouts.map((s) => (
+          <Shout key={s.id} {...s} />
+        ))}
       </div>
     </main>
   );

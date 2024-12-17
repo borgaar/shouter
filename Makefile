854aae191344458b2977effccd68404008fae1ba
Borgar Barland <142639383+borgaar@users.noreply.github.com>
2024-12-17 22:50:05 +0100

    improved devops dev experience

--- Makefile
+++ Makefile
@@ -1,12 +1,19 @@
 fresh:
 	- docker compose --profile dev -f docker-compose.yml down
-	- docker image rm shouter-shouter-dev
-	- docker volume rm shouter_shouter-data
+	- docker image rm shouter-web-app
+	- docker volume rm shouter_shouter_data
 	- docker compose --profile dev -f docker-compose.yml up -d
-	- sleep 1
+	- sleep 2
+	- npx prisma migrate dev
+
+db:
+	- docker compose --profile dev -f docker-compose.yml down database
+	-	docker volume rm shouter_shouter_data
+	- docker compose --profile dev -f docker-compose.yml up -d database
+	- sleep 2
 	- npx prisma migrate dev
 
 prod:
 	- docker compose --profile production -f docker-compose.yml down
-	- docker image rm ghcr.io/borgaar/shouter
+	- docker image rm ghcr.io/borgaar/shouter:latest
 	- docker compose --profile production -f docker-compose.yml up -d
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 import { auth } from "~/server/auth";
-import { api, } from "~/trpc/server";
+import { api } from "~/trpc/server";
 import Shout from "./_components/shout";
 import CreateShout from "./_components/create_shout";
 
@@ -11,17 +11,14 @@ export default async function Home() {
   const shouts = await api.post.getShouts(0); // TODO: pagination logic
 
   return (
-    <main className="bg-primary_dark flex min-h-screen flex-col items-center justify-center text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-primary_dark text-white">
       <div>
-        {session ? (
-          <Link href="/api/auth/signout">Sign out</Link>
-        ) : (
-          <Link href="/api/auth/signin">Sign in</Link>
-        )}
+        <Link href={session ? "/api/auth/signout" : "/api/auth/signin"}>
+          {session ? "Sign out" : "Sign in"}
+        </Link>
       </div>
-      {session && (<CreateShout/>
-        )}
-      <div className="bg-primary container flex h-screen max-w-screen-md flex-col items-center justify-center gap-12 px-4 py-16">
+      {session && <CreateShout />}
+      <div className="container flex h-screen max-w-screen-md flex-col items-center justify-center gap-12 bg-primary px-4 py-16">
         {shouts.map((s) => Shout(s))}
       </div>
     </main>
